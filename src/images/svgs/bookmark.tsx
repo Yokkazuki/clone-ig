7bc752a0ee5ae8cbfa6469ed2a56a08de061174b
Sammy Ma <sma990404@gmail.com>
2023-01-05 19:04:42 +0800

    add suggestion to home

--- src/images/svgs/bookmark.tsx
+++ src/images/svgs/bookmark.tsx
@@ -1,5 +1,5 @@
 const BookmarkSvg = () => {
-    return <svg aria-label="Save" color="#262626" fill="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24"><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></polygon></svg>
+    return <svg aria-label="Save" color="#262626" fill="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24"><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon></svg>
 }
 
 export default BookmarkSvg
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import BookmarkSvg from "../images/svgs/bookmark";
 import CommentSvg from "../images/svgs/comment";
 import HeartSvg from "../images/svgs/heart";
 import ShareSvg from "../images/svgs/share";
+import ThreeDotSvg from "../images/svgs/threeDot";
 import "./home.css"
 import Login from "./login";
 
@@ -12,9 +13,9 @@ type HomeProps = {
 const Home = (props: HomeProps) => {
     const dispatch = useDispatch();
     return <div>
-        <div className="developing">
+        {/* <div className="developing">
             Developing
-        </div>
+        </div> */}
         {!props.isLogin ? <Login /> :
             <div className="home">
                 <div className="main-field">
@@ -36,8 +37,11 @@ const Home = (props: HomeProps) => {
                     <div className="post-field">
                         <div className="post">
                             <div className="post-header">
-                                <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
-                                <div className="username">user</div>
+                                <div className="user">
+                                    <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                                    <div className="username">user</div>
+                                </div>
+                                <ThreeDotSvg />
                             </div>
                             <div className="post-image-container">
                                 <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
@@ -51,7 +55,88 @@ const Home = (props: HomeProps) => {
                                 </div>
                                 <div className="likes">1000 likes</div>
                                 <div className="post-description">
-                                    this is description
+                                    <div className="username">user</div> this is description
+                                </div>
+                                <div className="post-comment">
+                                    this is comment
+                                </div>
+                            </div>
+                        </div>
+                        <div className="post">
+                            <div className="post-header">
+                                <div className="user">
+                                    <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                                    <div className="username">user</div>
+                                </div>
+                                <ThreeDotSvg />
+                            </div>
+                            <div className="post-image-container">
+                                <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                            </div>
+                            <div className="post-bottom">
+                                <div className="post-action">
+                                    <HeartSvg />
+                                    <CommentSvg />
+                                    <ShareSvg />
+                                    <BookmarkSvg />
+                                </div>
+                                <div className="likes">1000 likes</div>
+                                <div className="post-description">
+                                    <div className="username">user</div> this is description
+                                </div>
+                                <div className="post-comment">
+                                    this is comment
+                                </div>
+                            </div>
+                        </div>
+                        <div className="post">
+                            <div className="post-header">
+                                <div className="user">
+                                    <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                                    <div className="username">user</div>
+                                </div>
+                                <ThreeDotSvg />
+                            </div>
+                            <div className="post-image-container">
+                                <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                            </div>
+                            <div className="post-bottom">
+                                <div className="post-action">
+                                    <HeartSvg />
+                                    <CommentSvg />
+                                    <ShareSvg />
+                                    <BookmarkSvg />
+                                </div>
+                                <div className="likes">1000 likes</div>
+                                <div className="post-description">
+                                    <div className="username">user</div> this is description
+                                </div>
+                                <div className="post-comment">
+                                    this is comment
+                                </div>
+                            </div>
+                        </div>
+                        <div className="post">
+                            <div className="post-header">
+                                <div className="user">
+                                    <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                                    <div className="username">user</div>
+                                </div>
+                                <ThreeDotSvg />
+                            </div>
+                            <div className="post-image-container">
+                                <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                            </div>
+                            <div className="post-bottom">
+                                <div className="post-action">
+                                    <HeartSvg />
+                                    <CommentSvg />
+                                    <ShareSvg />
+                                    <BookmarkSvg />
+                                </div>
+                                <div className="likes">1000 likes</div>
+                                <div className="post-description">
+                                    <div className="username">user</div> this is description
                                 </div>
                                 <div className="post-comment">
                                     this is comment
@@ -61,6 +146,66 @@ const Home = (props: HomeProps) => {
                     </div>
                 </div>
                 <div className="user-field">
+                    <div className="current-user">
+                        <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                        <div className="user">
+                            <div className="username">user</div>
+                            <div className="name">user</div>
+                        </div>
+                        <div className="switch-btn">Switch</div>
+                    </div>
+
+                    <div className="user-suggestions">Suggestions For You</div>
+                    <div className="suggestions-list">
+                        <div className="suggestion">
+                            <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                            <div className="user">
+                                <div className="username">user</div>
+                                <div className="followed-by">Followed by someone</div>
+                            </div>
+                            <div className="follow-btn">follow</div>
+                        </div>
+                        <div className="suggestion">
+                            <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                            <div className="user">
+                                <div className="username">user</div>
+                                <div className="followed-by">Followed by someone</div>
+                            </div>
+                            <div className="follow-btn">follow</div>
+                        </div>
+                        <div className="suggestion">
+                            <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                            <div className="user">
+                                <div className="username">user</div>
+                                <div className="followed-by">Followed by someone</div>
+                            </div>
+                            <div className="follow-btn">follow</div>
+                        </div>
+                        <div className="suggestion">
+                            <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                            <div className="user">
+                                <div className="username">user</div>
+                                <div className="followed-by">Followed by someone</div>
+                            </div>
+                            <div className="follow-btn">follow</div>
+                        </div>
+                        <div className="suggestion">
+                            <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                            <div className="user">
+                                <div className="username">user</div>
+                                <div className="followed-by">Followed by someone</div>
+                            </div>
+                            <div className="follow-btn">follow</div>
+                        </div>
+                        <div className="suggestion">
+                            <img src="https://static.displate.com/857x1200/displate/2022-07-07/fb201c5aef2a8558a1eec3a095be6d49_1c1023275f02c2ee7bc146309a812775.jpg" />
+                            <div className="user">
+                                <div className="username">user</div>
+                                <div className="followed-by">Followed by someone</div>
+                            </div>
+                            <div className="follow-btn">follow</div>
+                        </div>
+                    </div>
                 </div>
             </div>}
     </div>
